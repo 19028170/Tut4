87a8491b90750d423a298cfa06002e47b11c2d31
Josh Alexander <t1josh@outlook.com>
2020-08-24 16:24:52 +1200

    adding files

---
+++ src/main/java/tut4/Calc.java
@@ -0,0 +1,12 @@
+package tut4;
+
+public class Calc{
+	
+	public static int Add(int A, int B){
+		return A + B;
+	}
+	public static int Subtract(int A, int B){
+		return A + B;
+	}
+	
+}
---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,16 @@
+package tut4;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestCalc {
+	@Test
+	public void testAdd(){
+		int A = 5;
+		int B = 5;
+		int answer = A + B;
+		assertEquals(Calc.Add(A,B), answer);
+	}
+	
+}
